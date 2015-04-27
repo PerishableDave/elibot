daa8b40c2430cc8172650f3f9340eea56a1fbc51
David Peredo <daveperedo@gmail.com>
2015-04-27 07:53:49 +0000

    Add tests for socket.

--- test/elibot/socket_test.exs
+++ test/elibot/socket_test.exs
@@ -1,8 +1,41 @@
 defmodule Elibot.SocketTest do
   use ExUnit.Case
 
-  @start_response %{
-    "body" => "{\"url\": \"wss\"}"
-  }
+  alias Elibot.Socket
 
+  @socket_pid 123
+  @token "1234"
+  @url "https://slack.com/api/rtm.start?token=1234"
+
+  defmodule MockWebSocket do
+    def start_link("wss", module, args) do
+      {:ok, 123}
+    end
+
+    def start_link(_, module, args) do
+      {:error, "wrong arg for socket start link"}
+    end
+  end
+
+  defmodule MockHttp do
+    def get("https://slack.com/api/rtm.start?token=1234") do
+      {:ok, %HTTPoison.Response{status_code: 200,
+                                headers: [],
+                                body: "{\"url\": \"wss\"}"}}
+    end
+
+    def get(url) do
+      {:error, "wrong arg for http get: " <> url}
+    end
+  end
+
+  defmodule MockEventServer do
+  end
+
+  test "start event server" do
+    assert {:ok, @socket_pid} = Socket.start_link(@token,
+      MockWebSocket,
+      MockHttp,
+      MockEventServer)
+  end
 end
